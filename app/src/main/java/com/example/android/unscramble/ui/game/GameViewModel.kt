14ce6675037dec3192ce2c13361789d0a7c34752
Fakhry <ary@localhost.localdomain>
2021-12-03 10:12:43 +0900

    Delete unused code

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -48,11 +48,6 @@ class GameViewModel : ViewModel() {
         getNextWord()
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.d("GameFragment", "GameViewModel destroyed")
-    }
-
     /*
      * Updates currentWord and currentScrambledWord with the next word.
      */
